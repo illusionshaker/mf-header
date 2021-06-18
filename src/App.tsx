5aba7da951139ac9ef707073f1912a142fcb62d7
Mathew Ng <mathew.ng@iress.com>
2021-06-18 17:10:27 +1000

    initial commit of header

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Header from "./Header";
+import "./index.scss";
+
+const App = () => (
+  <div>
+    <Header />
+    <div>I'm the header app</div>
+  </div>
+);
+
+export default App;
